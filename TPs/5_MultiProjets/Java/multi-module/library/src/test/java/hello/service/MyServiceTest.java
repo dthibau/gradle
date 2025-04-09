144e2244cbb5e4f13be51f40ee757d80800b2431
David THIBAU <david.thibau@gmail.com>
2025-04-09 08:36:32 +0200

    Multi-module SB3.4.4

--- TPs/5_MultiProjets/Java/multi-module/library/src/test/java/hello/service/MyServiceTest.java
+++ TPs/5_MultiProjets/Java/multi-module/library/src/test/java/hello/service/MyServiceTest.java
@@ -2,14 +2,12 @@ package hello.service;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest("service.message=Hello")
 public class MyServiceTest {
 
